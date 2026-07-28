bad2c46d793a8e62aa3dad80b67c3348622fc6ed
Ahmet Yahya Yılmaz <ahmetyahyay@gmail.com>
2026-07-28 13:22:43 +0300

    Delete project_barber directory

--- project_barber/lib/app/routes.dart
+++
@@ -1,9 +0,0 @@
-abstract class Routes {
-  static const splash = '/';
-  static const roleSelect = '/role';
-  static const customerLogin = '/login/customer';
-  static const barberLogin = '/login/barber';
-  static const customerShell = '/customer';
-  static const barberShell = '/barber';
-}
-
